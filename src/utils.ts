4ab9448686f7355c5ce1704af09f264289b73f04
Pablo Rufat <pablorufatdev@gmail.com>
2022-05-17 01:04:12 +0200

    feat: add read sudoku file feature

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,74 @@
-import { Sudoku } from "./interfaces";
+import { squareIndexes, Sudoku } from "./interfaces";
+import * as fs from "fs";
 
-export const readFile = async (path: string): Promise<Sudoku> => {
-
+export const readSudokuFile = (path: string): Sudoku => {
+    try {
+        const data: string = fs.readFileSync(path, 'utf8');
+        formatFile(data);
+        const sudoku: Sudoku = formatFile(data);
+        return sudoku;
+    } catch (err) {
+        throw new Error("Error al leer el archivo.");
+    }
 }; 
+
+const formatFile = (data: string): Sudoku => {
+    const sudoku: Sudoku = {
+        lines: [],
+        columns: [],
+        squares: [],
+    };
+
+    const lines = data.split('\r\n');
+    
+    if (lines.length < 9) {
+        throw new Error("Formato incorrecto.");
+    }
+
+    sudoku.lines = getLines(lines);
+    sudoku.columns = getColumns(sudoku.lines);
+    sudoku.squares = getSquares(sudoku.lines);
+    
+    return sudoku;    
+};
+
+const getSquares = (lines: number[][]): number[][] => {
+    const parsedSquares: number[][] = [];
+
+    for (const squareIndex of squareIndexes) {
+        const square: number[] = [];
+        for (const index of squareIndex) {
+            square.push(lines[index[0]][index[1]]);
+        }
+        parsedSquares.push(square);
+    }
+
+    return parsedSquares;
+};
+
+const getLines = (lines: string[]): number[][] => {
+    const parsedLines: number[][] = [];
+
+    for (const line of lines) {
+        if (line.length > 0) {
+            const set: number[] = line.split(';').map(Number);
+            parsedLines.push(set);
+        }
+    }
+
+    return parsedLines;
+};
+
+const getColumns = (lines: number[][]): number[][] => {
+    const parsedColumns: number[][] = [];
+
+    for (let i = 0 ; i < 9 ; i++) {
+        const column: number[] = [];
+        for(const line of lines) {
+            column.push(line[i]);
+        }
+        parsedColumns.push(column);
+    }
+
+    return parsedColumns;
+};
